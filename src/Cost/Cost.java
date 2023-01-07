d03e94b8ada3cb101f3b3bdad37a09de259029d6
Eric <ericho@berkeley.edu>
2023-01-06 16:29:09 -0800

    fixed costs

--- src/Cost/Cost.java
+++ src/Cost/Cost.java
@@ -2,9 +2,9 @@ package Cost;
 
 public interface Cost {
 
-    public double CalcCost(double[] predictedOut, double[] expectedOut);
+    public double calcCost(double[] predictedOut, double[] expectedOut);
 
-    public double Derivative(double predictedOut, double expectedOut);
+    public double derivative(double predictedOut, double expectedOut);
 
     public Costs.CostType getCostType();
 }
--- src/Cost/Costs.java
+++ src/Cost/Costs.java
@@ -3,14 +3,16 @@ package Cost;
 public class Costs {
 
     public enum CostType {
-        MeanSquaredError,
-        CrossEntropy;
+        MeanSquaredError, CrossEntropy;
     }
 
-    public class MeanSquaredError implements Cost {
+    public static class MeanSquaredError implements Cost {
+
+        public MeanSquaredError() {
+        }
 
         @Override
-        public double CalcCost(double[] predictedOut, double[] expectedOut) {
+        public double calcCost(double[] predictedOut, double[] expectedOut) {
             double cost = 0.0;
             for (int i = 0; i < predictedOut.length; ++i) {
                 cost += Math.pow(predictedOut[i] - expectedOut[i], 2);
@@ -19,7 +21,7 @@ public class Costs {
         }
 
         @Override
-        public double Derivative(double predictedOut, double expectedOut) {
+        public double derivative(double predictedOut, double expectedOut) {
             return expectedOut - predictedOut;
         }
 
